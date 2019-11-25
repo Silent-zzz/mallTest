559eee2b74c30afdf2dca94a797291f0f910fffa
Silent-zzz <1044011060@qq.com>
2019-11-25 11:14:05 +0800

    tijn

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  fonfigureWebpack : {
+    resolve: {
+      alias: {
+        'assets' : '@/assets',
+        'common' : '@/common',
+        'components' : '@/components',
+        'network' : '@/network',
+        'views' : '@/views',
+      }
+    }
+  }
+}
